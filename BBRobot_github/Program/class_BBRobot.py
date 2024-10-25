d9ecca8926118c950e4513fe68bc0652bedb11a6
Huntermiki <huntermikimaster@yahoo.com>
2024-10-25 13:46:42 +0330

    translate coments

--- BBRobot_github/Program/class_BBRobot.py
+++ BBRobot_github/Program/class_BBRobot.py
@@ -2,45 +2,45 @@ import class_servo as cs
 import math
 import time
 
-class BBrobot:
-    #ロボットの構造を作成
-    def __init__(self, ids):#サーボ番号のリストを受け取る
-        #シリアルポートを準備
+class Robot:
+    # Create robot structure
+    def __init__(self, ids):# Receive servo number list
+        # Prepare serial port
         self.port = cs.sPort()
-        #サーボを準備
+        # Prepare servo
         self.s1 = cs.RS304MD(ids[0])
         self.s2 = cs.RS304MD(ids[1])
         self.s3 = cs.RS304MD(ids[2])
-        #リンクの長さL = [底, 下リンク, 上リンク, 天井]
+        # Link length L = [base, lower link, upper link, ceiling]
         self.L = [0.04, 0.04, 0.065, 0.065]
-        #初期姿勢(theta, phi, pz)
+        # Initial posture (theta, phi, pz)
         self.ini_pos = [0, 0, 0.0632]
         self.pz_max = 0.0732
         self.pz_min = 0.0532
         self.phi_max = 20
 
 
-    #ロボットの準備をするメソッド
+    # Prepare robot method
     def set_up(self):
-        #サーボのトルクをオン
+        # Turn on servo torque
         self.s1.trq_set(1)
         self.s2.trq_set(1)
         self.s3.trq_set(1)
         
     
-    #ロボットを片付けるメソッド
+    # Clean up robot method
     def clean_up(self):
-        #サーボのトルクをオフ
+        # Turn off servo torque
         self.s1.trq_set(0)
         self.s2.trq_set(0)
         self.s3.trq_set(0)
-        #シリアルポートを片付け
+        # Clean up serial port
         self.port.close_sPort()
 
-    #逆運動学を計算するメソッド
+    # Calculate inverse kinematics method
     def kinema_inv(self, n, Pz):
         L = self.L
-        #サーボ基準時の高さPmz導出(Pmzで+-反転)
+        # Calculate servo base height Pmz (Pmz with +/- inversion)
         A = (L[0]+L[1])/Pz
         B = (Pz**2+L[2]**2-(L[0]+L[1])**2-L[3]**2)/(2*Pz)
         C = A**2+1
@@ -49,7 +49,7 @@ class BBrobot:
         Pmx = (-D+math.sqrt(D**2-4*C*E))/(2*C)
         Pmz = math.sqrt(L[2]**2-Pmx**2+2*(L[0]+L[1])*Pmx-(L[0]+L[1])**2)
 
-        #サーボaの角度導出
+        # Calculate servo a angle
         a_m_x = (L[3]/(math.sqrt(n[0]**2 + n[2]**2)))*(n[2])
         a_m_y = 0
         a_m_z = Pz + (L[3]/(math.sqrt(n[0]**2 + n[2]**2)))*(-n[0])
@@ -67,7 +67,7 @@ class BBrobot:
         A_2 = [ax, ay, az]
         theta_a = 90 - math.degrees(math.atan2(A_2[0]-L[0], A_2[2]))
 
-        #サーボbの角度導出
+        # Calculate servo b angle
         b_m_x = (L[3]/(math.sqrt(n[0]**2+3*n[1]**2+4*n[2]**2+2*math.sqrt(3)*n[0]*n[1])))*(-n[2])
         b_m_y = (L[3]/(math.sqrt(n[0]**2+3*n[1]**2+4*n[2]**2+2*math.sqrt(3)*n[0]*n[1])))*(-math.sqrt(3)*n[2])
         b_m_z = Pz + (L[3]/(math.sqrt(n[0]**2+3*n[1]**2+4*n[2]**2+2*math.sqrt(3)*n[0]*n[1])))*(math.sqrt(3)*n[1]+n[0])
@@ -86,7 +86,7 @@ class BBrobot:
         B_2 = [x, y, z]
         theta_b = 90 - math.degrees(math.atan2(math.sqrt(B_2[0]**2+B_2[1]**2)-L[0], B_2[2]))
 
-        #サーボcの角度導出
+        # Calculate servo c angle
         c_m_x = (L[3]/(math.sqrt(n[0]**2+3*n[1]**2+4*n[2]**2-2*math.sqrt(3)*n[0]*n[1])))*(-n[2])
         c_m_y = (L[3]/(math.sqrt(n[0]**2+3*n[1]**2+4*n[2]**2-2*math.sqrt(3)*n[0]*n[1])))*(math.sqrt(3)*n[2])
         c_m_z = Pz + (L[3]/(math.sqrt(n[0]**2+3*n[1]**2+4*n[2]**2-2*math.sqrt(3)*n[0]*n[1])))*(-math.sqrt(3)*n[1]+n[0])
@@ -107,11 +107,11 @@ class BBrobot:
         thetas = [theta_a, theta_b, theta_c]
         return thetas
 
-    #t秒後に姿勢(theta, phi, Pz)を実現するメソッド
+    # t seconds later, realize the posture (theta, phi, Pz)
     def control_t_posture(self, pos, t):
         theta = pos[0]
         phi = pos[1]
-        #動作の制約
+        # Motion constraints
         if phi > self.phi_max:
             phi = self.phi_max
         Pz = pos[2]
